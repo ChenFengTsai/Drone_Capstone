429c2ed9864bb91c4bcecc6d7caeb4a9828d2ac9
mia0397 <128655019+mia0397@users.noreply.github.com>
2023-05-10 19:51:56 -0500

    Add files via upload

---
+++ demo/takeoff_voice.py
@@ -0,0 +1,40 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Wed May  3 20:44:19 2023
+
+@author: miaya
+"""
+
+
+from djitellopy import Tello
+import speech_recognition as sr
+
+
+def fly_drones_voice_1(drone1):
+
+    # Create recognizer
+    r = sr.Recognizer()
+    mic = sr.Microphone()
+
+    # Continuously listen for the oral command
+    while True:
+        with mic as source:
+            audio = r.listen(source, timeout = 10, phrase_time_limit = 3)
+        try:
+            
+            command = r.recognize_google(audio).lower()
+            print(command)
+            if "land" in command:
+                break
+            elif "apple" in command:
+                drone1.takeoff()
+        except sr.UnknownValueError:
+            pass
+        
+    
+    
+drone1 = Tello('192.168.86.27')
+drone1.connect()
+drone1.streamon()
+fly_drones_voice_1(drone1)
