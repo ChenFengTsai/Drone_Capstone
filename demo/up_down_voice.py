c5307c3defa19e7b48dcfb07b889d5b4c9b8f905
Chen Feng Tsai <rich.tsai1103@gmail.com>
2023-05-10 20:29:19 -0500

    main

---
+++ demo/up_down_voice.py
@@ -0,0 +1,38 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Wed May  3 20:44:19 2023
+
+@author: miaya
+"""
+from djitellopy import Tello
+import speech_recognition as sr
+
+
+import speech_recognition as sr
+
+class Voice_Detection:
+    def __init__(self, drone):
+        self.drone = drone
+        self.recognizer = sr.Recognizer()
+        self.microphone = sr.Microphone()
+
+    def fly_drones_voice(self):
+        with self.microphone as source:
+            audio = self.recognizer.listen(source, timeout=10, phrase_time_limit=3)
+
+        try:
+            command = self.recognizer.recognize_google(audio).lower()
+            print(command)
+
+            if "land" in command:
+                self.drone.land()
+            elif "takeoff" in command:
+                self.drone.takeoff()
+            else:
+                print("I don't understand the command")
+        except sr.UnknownValueError:
+            pass
+
+
+
--- main.py
+++ main.py
@@ -1,10 +1,12 @@
+from djitellopy import Tello
+import speech_recognition as sr
+from demo.up_down_voice import Voice_Detection
 
 
-
-def main():
+def main(drone1):
     # voice detection to fly
-    
-    
+    vd = Voice_Detection(drone1)
+    vd.fly_drones_voice()
     
     # gesture detection to trigger object detection
     
@@ -17,4 +19,9 @@ def main():
     pass
 
 if  __name__ == "__main__":
-    main()
+    # initilize the drone
+    drone1 = Tello('192.168.86.27')
+    drone1.connect()
+    drone1.streamon()
+    
+    main(drone1)
